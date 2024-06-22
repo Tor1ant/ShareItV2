59cd6917ecb3a95948d4a19e3add5ff2f5b9bba8
Vyacheslav <torlant312@yandex.ru>
2024-06-22 12:48:55 +0300

    initial commit

---
+++ src/main/java/com/github/tor1ant/shareitv2/ShareItV2Application.java
@@ -0,0 +1,13 @@
+package com.github.tor1ant.shareitv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareItV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareItV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/tor1ant/shareitv2/ShareItV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tor1ant.shareitv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareItV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
